08a51c06ff844b3f26bc699fdceb9784846de602
ntshashikumar <143063748+ntshashikumar@users.noreply.github.com>
2024-12-06 23:31:57 +0530

    Add files via upload

---
+++ TextToHandWritten.py
@@ -0,0 +1,5 @@
+import pywhatkit as pwt
+pwt.text_to_handwriting("Namaste Shashi","C:\\Users\\Admin\\Documents\\Shashi\\Python\\python\\output.png",[10,10,10])
+# f0=open("filename","r")
+# str = fo.read()
+# pwt.text_to_handwriting(str,"path\\ouput.png",[10,20,10])
